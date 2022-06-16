1ef871db7e7f2e5ab77baca17124e231ed368b46
lth888 <3214067981@qq.com>
2022-06-16 11:18:04 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,166 @@
+<template>
+  <el-container>
+    <el-header style="line-height: 45px; height: 45px;">
+      <el-image :src="require('@/assets/7.png')" style="width: 60px; height: 60px; position: fixed"> </el-image>
+      <b class="top_left_text">猫头鹰助理</b>
+    </el-header>
+    <el-divider class="el-divider--horizontal"></el-divider>
+    <el-main style="line-height: 400px;">
+      <el-row>
+        <el-col :span="8">
+          <div class="grid-content bg-white"></div>
+        </el-col>
+        <el-col :span="8" style="margin-top: 80px">
+          <div>
+            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm">
+              <el-form-item>
+                <el-row style="text-align: center">
+                  <b style="font-size: 32px; margin-bottom: 0"> 登录 </b>
+                </el-row>
+              </el-form-item>
+              <el-form-item label="工号" prop="userName">
+                <el-input style="width: 400px" placeholder="请输入内容" v-model="ruleForm.userName"
+                          autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input style="width: 400px" placeholder="请输入内容" type="password" v-model="ruleForm.password"
+                          autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-row style="text-align: center">
+                  <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                </el-row>
+              </el-form-item>
+              <el-form-item>
+                <el-row style="text-align: center">
+                  <router-link to="/OA">OA登录</router-link>
+                </el-row>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+        <el-col :span="8">
+          <el-image :src="require('@/assets/7.png')"
+                    style="width: 343px; height: 450px" class="bg_picture">
+          </el-image>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    var validateId = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入工号'))
+      } else {
+        callback()
+      }
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      ruleForm: {
+        userName: '',
+        password: ''
+      },
+      rules: {
+        userName: [
+          {validator: validateId, trigger: 'blur'}
+        ],
+        password: [
+          {validator: validatePass, trigger: 'blur'}
+        ]
+      },
+      pass: {
+        user_id: '',
+        isSucceed: '1',
+        token: ''
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // 向后端请求用户认证
+          this.$http.post('http://localhost:8989/user/login', this.ruleForm).then(res => {
+            // console.log(res.data)
+            if (res.data.code === 200) {
+              // alert(res.data.msg)
+              localStorage.setItem('token', res.data.data.token)
+              localStorage.setItem('userName', this.ruleForm.userName)
+              this.$router.push('/main')
+            } else if (res.data.code === 401) {
+              alert('用户名或密码错误，请重试')
+            } else {
+              alert('请勿重复登录')
+            }
+          })
+        } else {
+          alert('用户名或密码不能为空')
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+
+.el-col.last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.bg-white {
+  background: #ffffff
+}
+
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+
+.bg_picture {
+  left: 30%;
+  opacity: 60%;
+}
+
+.el-divider--horizontal {
+  display: block;
+  height: 1px;
+  width: 100%;
+  margin: 20px 0px;
+}
+.top_left_text{
+  position: absolute;
+  left: 80px;
+  margin: 10px;
+}
+</style>
---
+++ src/components/PhoneAssist.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }" class="el-icon-s-home"></el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/#/Main/phoneAssist">电话辅销</a></el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row><br></el-row>
+    <el-row>
+      <el-col :span="4"><el-button type="primary" @click="translationStart">开始对话</el-button>  <i class="el-icon-microphone"></i></el-col>
+      <el-col :span="4"><el-button type="primary" @click="translationEnd">结束对话</el-button></el-col>
+      <el-col :span="4"></el-col>
+      <el-col :span="4"></el-col>
+      <el-col :span="4"></el-col>
+      <el-col :span="4"></el-col>
+    </el-row>
+    <br>
+    <div class="communicate_box">
+      <el-scrollbar style="height:100%">
+      <el-card class="box-card">
+        <el-card class="box-card" v-for="item in list" :key="item">
+          <div class="text item">
+            {{item}}
+          </div>
+        </el-card>
+      </el-card>
+      </el-scrollbar>
+    </div>
+  </div>
+</template>
+
+<script>
+import IatRecorder from '@/assets/js/IatRecorder.js'
+import axios from "axios";
+var text_list = []
+//初始化iatRecorder
+const iatRecorder = new IatRecorder({
+  accent:'mandarin',
+  onTextChange: function (text) {
+    if (text.length < 3)
+      return
+    text_list.push(text)
+    axios.get('http://localhost:8989/recommend?str=' + text, {
+      headers: {
+        'token': localStorage.getItem('token')
+      }
+    }).then(res => {
+      if (res.data.code === 200) {
+        console.log(res.data.data.content);
+      }
+    })
+}})
+
+export default {
+  name: 'PhoneAssist',
+  data () {
+    return {
+      list: text_list,
+    }
+  },
+  methods: {
+    translationStart() {
+      //启动语音识别
+      iatRecorder.start()
+    },
+    translationEnd() {
+      //结束对话
+      iatRecorder.stop()
+    }
+  },
+}
+</script>
+
+<style>
+.communicate_box{
+  overflow: hidden;
+  margin: 0 auto;
+  width: auto;
+  height: 570px;
+  color: #B3C0D1;
+  /*margin: 0 auto;*/
+}
+
+/*.scrollMenuBox {*/
+/*  height: 200px;*/
+/*  width: 100px;*/
+/*  border: 1px solid red;*/
+/*}*/
+
+/* 需要在外层套一个div,切记命名特殊一点,防止因缺少scoped，对其他页面会有冲突 */
+.center .el-scrollbar__wrap {
+  overflow-x: hidden;
+}
+</style>
